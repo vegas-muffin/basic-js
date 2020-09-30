3a307358505db8510a386f66c70084057ee96268
Vagiz Musin <vagiz_mus@mail.ru>
2020-09-30 16:31:01 +0600

    feat: add count-cat task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  let cat = "^^";
+  let k = 0;
+  for (i = 0; i < backyard.length; i++) {
+    for (j = 0; j < backyard[i].length; j++) {
+      if (cat === backyard[i][j]) {
+        k++;
+      }
+    }
+  }
+  return k;
 };
