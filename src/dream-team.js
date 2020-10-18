33483d76ee39c2a91bc52e14d8dc1e935471b9fe
Vagiz Musin <vagiz_mus@mail.ru>
2020-10-18 23:24:26 +0600

    feat: add task dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let arr = [];
+  if (members === undefined ||members === null ||   members.length === 0) { 
+    return false;
+  } else {
+for (let i=0; i<members.length; i++) {
+    if (typeof(members[i]) === 'string') {
+      let str = members[i].trim().toUpperCase();    
+      arr.push(str.charAt(0));
+      }
+  }
+  if (arr.length === 0) { return false} else {return arr.sort().join('');}  
+  }
 };
