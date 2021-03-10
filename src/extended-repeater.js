ff9c1b20e1aaf0576970d48795fb5096b02981f3
Vagiz Musin <vagiz_mus@mail.ru>
2021-03-10 15:45:50 +0600

    feat: add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,34 @@
-const CustomError = require("../extensions/custom-error");
+ const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
-  
+      module.exports = function repeater(str, options) {
+        if (!options.hasOwnProperty("separator")) options.separator = "+";
+        if (!options.hasOwnProperty("additionSeparator"))
+          options.additionSeparator = "||";
+
+        if (
+          options.hasOwnProperty("repeatTimes") &&
+          options.repeatTimes == undefined
+        ) {
+          options.repeatTimes = 1;
+        }
+        if (
+          options.hasOwnProperty("additionRepeatTimes") &&
+          options.additionRepeatTimes == undefined
+        ) {
+          options.additionRepeatTimes = 1;
+        }
+        if (options.hasOwnProperty("addition") && options.addition == null) {
+          options.addition = "null";
+        }
+
+        let arr = Array(options.additionRepeatTimes)
+          .fill()
+          .map((i) => options.addition);
+        let str2 = arr.join(options.additionSeparator);
+
+        let resolve = Array(options.repeatTimes)
+          .fill()
+          .map((i) => str + str2);
+
+        return resolve.join(options.separator);
+      };
