a8b302d9af39cbf03c9ccd8dbf1d01bf6e797644
Vagiz Musin <vagiz_mus@mail.ru>
2021-03-14 19:57:57 +0600

    fix: fix

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,25 +1,46 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  if (!Array.isArray(arr)) throw new Error("Wrong Array!!!");
-  let arr2 = [];
-  for (let i = 0; i < arr.length; i++) {
-    switch (arr[i]) {
-      case '--discard-prev':
-        if (arr2.length && arr[i-2] !== '--discard-next') arr2.pop();
-        break;
-      case '--discard-next':
-        i++;
-        break;
-      case '--double-prev':
-        if (i && arr[i-2] !== '--discard-next') arr2.push(arr[i-1]);
-        break;
-      case '--double-next':
-        if (i < arr.length-1){} arr2.push(arr[i+1]);
-        break;
-      default:
-        arr2.push(arr[i]);
-    };
-  };
-  return arr2;
+  let outputA = [];
+  let discardDouble = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
+
+  for (let i=0; i<arr.length; i++) {
+    let pattern = arr[i];
+    if (discardDouble.includes(pattern)){
+      if (pattern === '--discard-prev') {
+        if ((outputA[outputA.length-1] !== undefined)&&(arr[i-1] !== undefined)) {
+          if ((outputA[outputA.length-1] === arr[i-1])||((isNaN(outputA[outputA.length-1])) && (isNaN(arr[i-1])))) {
+            if (arr[i-2] !== undefined) {
+              if (arr[i-2] !== '--discard-next'){
+                outputA.pop();
+              }
+            } else {
+              outputA.pop();
+            }
+          }
+        }
+      } else if (pattern === '--double-next'){
+        if (arr[i+1] !== undefined) {
+          outputA.push(arr[i+1]);
+        }
+      } else if (pattern === '--double-prev'){
+        if ((outputA[outputA.length-1] !== undefined)&&(arr[i-1] !== undefined)) {          
+          if ((outputA[outputA.length-1] == arr[i-1])||((isNaN(outputA[outputA.length-1])) && (isNaN(arr[i-1])))) {
+            if (arr[i-2] !== undefined) {
+              if (arr[i-2] !== '--discard-next'){
+                outputA.push(arr[i-1]);
+              }
+            } else {
+              outputA.push(arr[i-1]);
+            }
+          }
+        }
+      } else if (pattern === '--discard-next'){
+          i+=1;      
+      }
+    } else {  
+      outputA.push(pattern);
+    }
+  }
+  return outputA;
 };
