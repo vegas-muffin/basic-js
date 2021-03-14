52566386b36529f63458b1774f5272de2a794392
Vagiz Musin <vagiz_mus@mail.ru>
2021-03-14 18:59:58 +0600

    fix: fix

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,44 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
- if (arr.length === 0) {
-    return arr;
-  } else if (!Array.isArray(arr)) { 
-     throw new Error('Error - Not Array');
-  } else {
-      let str = arr.toString().split(',');
-      let newStr = [];
-        for (let i = 0; i < str.length; i++) {
-          if (str[i] === "--double-next") {
-              let doubleNext = str[i+1];
-              newStr.push(doubleNext);
-          } else if (str[i] === "--double-prev")  {
-              let doublePrev = str[i-1];
-              newStr.push(doublePrev);
-          } else if (str[i] === "--discard-prev")  {
-              let discardPrev = i-1;
-              delete newStr[discardPrev];
-          } else if (str[i] === "--discard-next")  {
-              let discardNext = i+1;
-              delete str[discardNext];
-          } else {
-              newStr.push(str[i]);
-          }
-          
-        }
-          function newArr(arr) {
-            newStr2 = [];
-            for (let i = 0; i < arr.length; i++) {
-              if (arr[i] === "--discard-next" || arr[i] === "--discard-prev" || arr[i] === "--double-prev" || arr[i] === "--double-next" || !arr[i]) {
-                continue;
-              } else {
-                newStr2.push(arr[i]);
-              }
-            }
-            // console.log(newStr2);
-            return newStr2;
-            
-          }
-          newArr(newStr);
-        }
+  if (!Array.isArray(arr)) throw new Error("Wrong Array!!!");
+  let arr2 = [];
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case '--discard-prev':
+        if (arr2.length && arr[i-2] !== '--discard-next') arr2.pop();
+        break;
+      case '--discard-next':
+        i++;
+        break;
+      case '--double-prev':
+        if (i && arr[i-2] !== '--discard-next') arr2.push(arr[i-1]);
+        break;
+      case '--double-next':
+        if (i < arr.length-1){} arr2.push(arr[i+1]);
+        break;
+      default:
+        arr2.push(arr[i]);
+    };
+  };
+  return arr2;
 };
