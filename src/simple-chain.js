3425b7c8e8196576271920b11aa69127abb9bdba
Vagiz Musin <vagiz_mus@mail.ru>
2021-03-14 19:34:37 +0600

    fix: fix

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,38 +1,34 @@
       const CustomError = require("../extensions/custom-error");
-
-      const chainMaker = {
-        mainChain: [],
-
-        getLength() {
-          return this.mainChain.length;
-        },
-        addLink(value) {
-          if (value === undefined) {
-            this.mainChain.push(" ");
-          } else if (value === null) {
-            this.mainChain.push("null");
-          } else {
-            this.mainChain.push(value.toString());
-          }
-          return this;
-        },
-        removeLink(position) {
-          if (position <= 0 || position > this.mainChain.length) {
-            this.mainChain = [];
-            throw new Error();
-          }
-          this.mainChain.splice(position - 1, 1);
-          return this;
-        },
-        reverseChain() {
-          this.mainChain = this.chain.reverse();
-          return this;
-        },
-        finishChain() {
-          let chained = this.mainChain.map((value) => `( ${value} )`);
-          this.mainChain = [];
-          return chained.join("~~");
-        },
-      };
+      const chainMaker =  {
+         mainChain: [],
+         getLength() {
+           return this.mainChain.length; 
+         },
+         addLink(value) {
+               if(value==null)
+             this.mainChain.push("( null )");
+                     else
+           this.mainChain.push("( "+value+" )");
+               return this;
+         },
+         removeLink(position) {
+               if(typeof position!=="number"||isNaN(position)||!Number.isInteger(position)||position>this.mainChain.length||position<1){
+                     this.mainChain.splice(0,this.mainChain.length);
+           throw new Error('Error!!!');
+               }
+               else
+        this.mainChain.splice(parseInt(position)-1,1);
+               return this;
+         },
+         reverseChain() {
+               this.mainChain.reverse();
+               return this;
+         },
+         finishChain() {        
+         let chain=this.mainChain.join("~~");
+               this.mainChain.splice(0,this.mainChain.length);
+           return chain;
+         }
+       };
 
       module.exports = chainMaker;
